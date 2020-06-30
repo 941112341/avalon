990dcb7f6e1742b017cb0972c885ecf13c641913
jiangshihao <jiangshihao@bytedance.com>
2020-06-30 15:14:24 +0800

    feat: 基础http/https服务
    Change-Id: Ie24f392f5365662b2108ac6e329753996901f674

--- gateway/main.go
+++ gateway/main.go
@@ -1,8 +1,50 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"sort"
+)
+
+// order 约小越先执行
+var Initializers InitializersSorted
+
+type Initializer struct {
+	Order   int
+	Name    string
+	Initial func(ctx context.Context) error
+}
+
+type InitializersSorted []Initializer
+
+func (sorted InitializersSorted) Len() int {
+	return len(sorted)
+}
+
+func (sorted InitializersSorted) Swap(i, j int) {
+	sorted[i], sorted[j] = sorted[j], sorted[i]
+}
+
+func (sorted InitializersSorted) Less(i, j int) bool {
+	return sorted[i].Order < sorted[j].Order
+}
+
+func init() {
+
+}
 
 func main() {
+
+	ctx := context.Background()
+	sort.Sort(Initializers)
+	for _, initializer := range Initializers {
+		if err := initializer.Initial(ctx); err != nil {
+			logrus.Errorf("initial err, task name %s, err %s", initializer.Name, err)
+			return
+		}
+	}
+
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("hello world"))
 	})
